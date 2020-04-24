ef332a8291a2b40bfbdb5e9bd7a93c918d4f620a
unknown <pangeriz@outlook.com>
2020-04-24 18:14:42 +0100

    local changes uploaded

--- code.js
+++ code.js
@@ -1,228 +1,210 @@
+
+// We ensure that the HTML is parsed when we attach listeners 
+
 if (document.readyState == 'loading') {
-
-    document.addEventListener('DOMContentLoaded', ready)
-
+    document.addEventListener('DOMContentLoaded', attachListeners)
 } else {
-
-    ready()
-
+    attachListeners()
 }
 
+function attachListeners() {
+
+    // We want to listen to remove item from cart buttons so keep the cart updated
+
+    const removeCartItemButtons = document.querySelectorAll('.btn-danger')
+
+    removeCartItemButtons.forEach(button => button.addEventListener('click', removeCartItem))
+
+    // We need to know if the user has typed a new quantity so we update the cart acordingly 
+
+    const quantityInputs = document.querySelectorAll('.cart-quantity-input')
+
+    quantityInputs.forEach(input => input.addEventListener('change', quantityChanged))
+
+    //If the user clicks on the add to  cart button the cart list is updated 
+
+    const addToCartButtons = document.querySelectorAll('.shop-item-button')
+
+    addToCartButtons.forEach(button => button.addEventListener('click', addToCartClicked))
 
 
-function ready() {
+    // We want to let users know if the purchase has been done when they clicks on the purchase button
 
-    let removeCartItemButtons = document.getElementsByClassName('btn-danger')
-
-    for (let i = 0; i < removeCartItemButtons.length; i++) {
-
-        let button = removeCartItemButtons[i]
-
-        button.addEventListener('click', removeCartItem)
-
-    }
-
-
-
-    let quantityInputs = document.getElementsByClassName('cart-quantity-input')
-
-    for (let i = 0; i < quantityInputs.length; i++) {
-
-        let input = quantityInputs[i]
-
-        input.addEventListener('change', quantityChanged)
-
-    }
-
-
-
-    let addToCartButtons = document.getElementsByClassName('shop-item-button')
-
-    for (let i = 0; i < addToCartButtons.length; i++) {
-
-        let button = addToCartButtons[i]
-
-        button.addEventListener('click', addToCartClicked)
-
-    }
-
-
-
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
-
+    const purchaseButton = document.querySelector('.btn-purchase')
+    purchaseButton.addEventListener('click', purchaseClicked)
 }
-
-
 
 function purchaseClicked() {
 
+    // Purchase notification for users
+
     alert('Thank you for your purchase')
 
-    let cartItems = document.getElementsByClassName('cart-items')[0]
+    // Then empty the basquet/cart
 
-    while (cartItems.hasChildNodes()) {
+    emptyTheCart()
 
-        cartItems.removeChild(cartItems.firstChild)
-
-    }
+    // And update the basquet/cart total amount
 
     updateCartTotal()
-
 }
 
+function emptyTheCart() {
 
+    // We then remove all items that were listed in the basquet/cart
+
+    const cartItems = document.querySelector('.cart-items')
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+
+}
 
 function removeCartItem(event) {
 
-    let buttonClicked = event.target
+    // Removal of items from the basquet/cart when 'remove' button is clicked 
 
+    const buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
 
+    // After this the basquet/cart amount is updated
+
     updateCartTotal()
-
 }
-
-
 
 function quantityChanged(event) {
+    const input = event.target
 
-    let input = event.target
+    // If the input is not valid we just set it to one 
 
     if (isNaN(input.value) || input.value <= 0) {
-
         input.value = 1
-
     }
 
+    // If the quantity of any item changes, the total amount of the cart changes accordingly 
+
     updateCartTotal()
-
 }
-
-
 
 function addToCartClicked(event) {
 
-    let button = event.target
+    // Get the info of the item that has been added 
 
-    let shopItem = button.parentElement.parentElement
+    const button = event.target
+    const shopItem = button.parentElement.parentElement
+    const title = shopItem.querySelector('.shop-item-title').innerText
+    const price = shopItem.querySelector('.shop-item-price').innerText
+    const imageSrc = shopItem.querySelector('.shop-item-image').src
 
-    let title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-
-    let price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-
-    let imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    // Then send the info over this function so it can be created and added
 
     addItemToCart(title, price, imageSrc)
 
-    updateCartTotal()
+    // We update the total amount of the cart 
 
+    updateCartTotal()
 }
 
+function itemAlreadyExists(title) {
 
+    // We get the list of items already in the cart 
+
+    const cartItems = document.getElementsByClassName('cart-items')[0]
+    const cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+
+    // And iterate over them to see if the parameter of the function already exists 
+
+    for (let i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            return true
+        }
+    }
+    return false
+}
 
 function addItemToCart(title, price, imageSrc) {
 
-    let cartRow = document.createElement('div')
+    // We use the helper function we just defined to avoid duplicates 
 
-    cartRow.classList.add('cart-row')
-
-    let cartItems = document.getElementsByClassName('cart-items')[0]
-
-    let cartItemNames = cartItems.getElementsByClassName('cart-item-title')
-
-    for (let i = 0; i < cartItemNames.length; i++) {
-
-        if (cartItemNames[i].innerText == title) {
-
-            alert('This item is already added to the cart')
-
-            return
-
-        }
-
+    if (itemAlreadyExists(title)) {
+        alert('This item is already added to the cart')
+        return
     }
 
-    let cartRowContents = `
-    
-    <div class="cart-item cart-column">
-    
-    
-    
-    <span class="cart-item-title">${title}</span>
-    
-    </div>
-    
-    <span class="cart-price cart-column">${price}</span>
-    
-    <div class="cart-quantity cart-column">
-    
-    <input class="cart-quantity-input" type="number" value="1">
-    
-    <button class="btn btn-danger" type="button">REMOVE</button>
-    
-    </div>`
+    // We create the node that we'll insert in the DOM 
 
+    const cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    const cartRowContents = `
+        <div class="cart-item cart-column">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
     cartRow.innerHTML = cartRowContents
 
+    // We get the cart and insert in it the new item 
+
+    const cartItems = document.getElementsByClassName('cart-items')[0]
     cartItems.append(cartRow)
 
+    // We also add buttons so the quantity can be changed and can be removed
+
     cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-
     cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
-
 }
-
-
 
 function updateCartTotal() {
 
-    let cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    // We get the cart 
 
-    let cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    const cartItemContainer = document.getElementsByClassName('cart-items')[0]
+
+    // We need the items in the cart to sum their prices 
+
+    const cartRows = cartItemContainer.getElementsByClassName('cart-row')
+
+    // Variable in which we'll acumulate the prices 
 
     let total = 0
 
+    // We iterate over the items 
+
     for (let i = 0; i < cartRows.length; i++) {
 
-        let cartRow = cartRows[i]
+        // We store the quantity and price of each item
 
-        let priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        const cartRow = cartRows[i]
+        const priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        const quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
 
-        let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        // We need the raw price without any weird characters
 
-        let price = parseFloat(priceElement.innerText.replace('£', ''))
+        const price = parseFloat(priceElement.innerText.replace('£', ''))
 
-        let quantity = quantityElement.value
+        // We can compute the total of each item using its price and quantity 
 
+        const quantity = quantityElement.value
         total = total + (price * quantity)
-
     }
-
     total = Math.round(total * 100) / 100
 
+    // Finally  we convert it into a string again 
     document.getElementsByClassName('cart-total-price')[0].innerText = '£' + total
-
 }
 
 
-// Clear contents of comment box on form
+
+// Clear contents of comment box on Contact form
 function clearContents(element) {
     element.value = '';
 }
 
-// Thank you message on form
+// Thank you message on Contact form
 function FormJS() {
     let jFirst = document.getElementById("fname").value;
     alert("Thank you for getting in touch with us " + jFirst);
 }
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon
-function myFunction() {
-    let x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-*/
