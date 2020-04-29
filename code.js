a4a86cd71772abdbad7bf0a27795532b95eca77d
unknown <pangeriz@outlook.com>
2020-04-29 21:07:42 +0100

    media queries and 'Remove' button

--- code.js
+++ code.js
@@ -142,7 +142,7 @@ function addItemToCart(title, price, imageSrc) {
         <span class="cart-price cart-column">${price}</span>
         <div class="cart-quantity cart-column">
             <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">REMOVE</button>
+            <button class="btn btn-danger" type="button">Remove</button>
         </div>`
     cartRow.innerHTML = cartRowContents
 
